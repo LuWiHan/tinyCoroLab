72d0ec1777e17a817890d221fcea078d8f0448da
liuweihuan <1253576153@qq.com>
2025-09-02 12:17:28 +0800

    ✨ feat: 实现lab4b

--- include/coro/comp/latch.hpp
+++ include/coro/comp/latch.hpp
@@ -11,8 +11,15 @@
 #pragma once
 
 #include <atomic>
+#include <cstdint>
+#include <mutex>
+#include <vector>
 
+#include "coro/comp/when_all.hpp"
 #include "coro/detail/types.hpp"
+#include "coro/meta_info.hpp"
+#include "coro/spinlock.hpp"
+#include "coro/context.hpp"
 
 namespace coro
 {
@@ -37,16 +44,63 @@ namespace coro
 // but keep the function count_down() and wait()'s declaration same with example.
 class latch
 {
+    struct waitting_element
+    {
+        context* ctx;
+        std::coroutine_handle<> handle;
+    };
+
+    struct awaiter
+    {
+        awaiter(latch* l) : m_latch(l){}
+        auto await_ready() -> bool { return m_latch->m_flag.load(std::memory_order_relaxed); }
+        auto await_suspend(std::coroutine_handle<> h) -> bool
+        {
+            std::lock_guard<detail::spinlock> lock(m_latch->m_lock);
+            if(m_latch->m_flag.load(std::memory_order_acquire))
+                return false;
+            waitting_element element;
+            element.ctx = detail::linfo.ctx;
+            element.handle = h;
+            m_latch->m_wait_queue.push_back(element);
+            m_register_cnt = 1;
+            element.ctx->register_wait(m_register_cnt);
+            return true;
+        }
+        auto await_resume() -> void { detail::linfo.ctx->unregister_wait(m_register_cnt); }
+
+        latch*  m_latch;
+        int     m_register_cnt{0};
+    };
 public:
-    latch(std::uint64_t count) noexcept {}
+    latch(std::uint64_t count) noexcept
+        : m_cnt(count) {}
     latch(const latch&)                    = delete;
     latch(latch&&)                         = delete;
     auto operator=(const latch&) -> latch& = delete;
     auto operator=(latch&&) -> latch&      = delete;
 
-    auto count_down() noexcept -> void {}
+    auto count_down() noexcept -> void
+    {
+        uint64_t cnt = m_cnt.fetch_sub(1,std::memory_order_acquire);
+        if(cnt == 1) // 第一个计数减0
+        {
+            // 1.设置标值
+            m_flag.store(true,std::memory_order_acquire);
+            // 2.唤醒协程
+            std::lock_guard<detail::spinlock> lock(m_lock);
+            for(auto& e:m_wait_queue)
+                e.ctx->submit_task(e.handle);
+        }
+        return;
+    }
 
-    auto wait() noexcept -> detail::noop_awaiter { return {}; }
+    auto wait() noexcept -> awaiter { return {this}; }
+private:
+    std::atomic_uint64_t            m_cnt;
+    std::atomic_bool                m_flag{false};
+    detail::spinlock                m_lock;
+    std::vector<waitting_element>   m_wait_queue;
 };
 
 /**
